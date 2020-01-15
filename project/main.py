3a8bea2f1bd7b2d04314a8012d5fd54c8709d38e
glazko1 <glazko_2001@mail.ru>
2020-01-15 21:48:19 +0300

    Add close word predictions

--- project/main.py
+++ project/main.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 
 data = json.load(open('../data/dictionary.json'))
@@ -6,14 +7,23 @@ data = json.load(open('../data/dictionary.json'))
 
 def get_definition(word):
     word = word.lower()
+    close_matches = get_close_matches(word, data.keys(), cutoff=0.8)
     if word in data:
         return data[word]
-    else:
-        return ['No such word in a dictionary!']
+    elif len(close_matches) > 0:
+        close_match = close_matches[0]
+        if suggest_close_match(close_match):
+            return data[close_match]
+    return ['No such word in a dictionary!']
+
+
+def suggest_close_match(predicted_word):
+    agree = input('Maybe you\'ve meant "' + predicted_word + '"? Type Y if yes, N otherwise: ')
+    return agree == 'Y'
 
 
 if __name__ == '__main__':
-    key = input('Enter a word: ')
+    key = input('Enter a word to get its definition: ')
     definition_list = get_definition(key)
     for definition in definition_list:
         print(definition)
